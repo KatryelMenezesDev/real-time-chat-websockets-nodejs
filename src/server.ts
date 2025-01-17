1dc3b361e744038163258db9f328201dbeb6c068
Katryel Menezes <katryelmenezesvieira14@gmail.com>
2025-01-17 15:02:44 -0300

    adiciona tsx como dependência de desenvolvimento e ajusta mensagem de inicialização do servidor

--- src/server.ts
+++ src/server.ts
@@ -6,14 +6,14 @@ class App{
     private app: express.Application
     private http: http.Server
     private io: Server
-    
+
     constructor(){
         this.app = express()
         this.http = http.createServer(this.app)
         this.io = new Server(this.http)
     }
     listenServer(){
-
+        this.http.listen(3000, () => { console.log('Server running on port 3000') })
     }
 }
 
